c81d8825dbc022a75c51f223fdad85dda9cfb968
LMitchell <mitchelllindgren8@gmail.com>
2023-08-30 10:45:31 -0700

    Update SocketProtocol.cs

--- SocketCommunication/SocketProtocol.cs
+++ SocketCommunication/SocketProtocol.cs
@@ -13,6 +13,7 @@ namespace SocketCommunication
 {
     internal class SocketProtocol
     {
+        
         #region Variables
         // ToDo: Rename variable correctly, using "_" 
         //public static bool _connection;
